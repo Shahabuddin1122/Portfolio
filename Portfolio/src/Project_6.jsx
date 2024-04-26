937c035f0e86a5cbd3186030544319f83363870b
Shahabuddin1122 <shavoddin54@gmail.com>
2024-04-26 14:18:39 +0600

    add project-7 video

--- Portfolio/src/Project_6.jsx
+++ Portfolio/src/Project_6.jsx
@@ -1,27 +1,54 @@
 import style from "./Project.module.css";
 
 const Project6 = () => {
-    return (
-        <div className={style.project1}>
-            <div className={style.project1child} />
-            <div className={style.project1item} ><p>Software</p></div>
-            <div className={style.project1inner} />
-            <div className={style.project1child1} > <img src="https://res.cloudinary.com/dkotituzn/image/upload/v1713619474/shahabuddin/bgklbv04vbpqefs53vd3.png" alt="" /></div>
-            <div className={style.project1child2} > <video src="https://res.cloudinary.com/dkotituzn/video/upload/v1713619589/shahabuddin/rhcke1motjaaapkqaxxd.mp4" controls muted autoPlay /></div>
-            <img className={style.subtracticon} alt="" src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596429/shahabuddin/eng49exy2sdyj5nlxjga.svg" />
-            <img className={style.subtracticon1} alt="" src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596427/shahabuddin/mzbr5u53amxf2w1iwe0o.svg" />
-            <b className={style.project}>Project</b>
-            <div className={style.thiscurtainopener}>{`A WhatsApp Clone was developed using Next.js and Java Spring Boot. Users log in with their mobile numbers, authenticated via Twilio.`}</div>
-            <div className={style.openandclose}>
-                Users can chat, supported by a pull-based architecture, accommodating  users messages seamlessly.
-            </div>
-            <div className={style.div}>04</div>
-            <div className={style.tempcurtainautomator}>WhatsApp <br/> Clone</div>
-            <div className={style.anarduinoproject}>
-                The platform enables seamless, secure messaging between users.
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.project1}>
+      <div className={style.project1child} />
+      <div className={style.project1item}>
+        <p>Software</p>
+      </div>
+      <div className={style.project1inner} />
+      <div className={style.project1child1}>
+        <img
+          src="https://res.cloudinary.com/dkotituzn/image/upload/v1713619474/shahabuddin/bgklbv04vbpqefs53vd3.png"
+          alt=""
+        />
+      </div>
+      <div className={style.project1child2}>
+        <video
+          src="https://res.cloudinary.com/dkotituzn/video/upload/v1714112902/shahabuddin/aii28jhqs2gkn6qubje1.mp4"
+          controls
+          muted
+          autoPlay
+        />
+      </div>
+      <img
+        className={style.subtracticon}
+        alt=""
+        src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596429/shahabuddin/eng49exy2sdyj5nlxjga.svg"
+      />
+      <img
+        className={style.subtracticon1}
+        alt=""
+        src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596427/shahabuddin/mzbr5u53amxf2w1iwe0o.svg"
+      />
+      <b className={style.project}>Project</b>
+      <div
+        className={style.thiscurtainopener}
+      >{`A WhatsApp Clone was developed using Next.js and Java Spring Boot. Users log in with their mobile numbers, authenticated via Twilio.`}</div>
+      <div className={style.openandclose}>
+        Users can chat, supported by a pull-based architecture, accommodating
+        users messages seamlessly.
+      </div>
+      <div className={style.div}>04</div>
+      <div className={style.tempcurtainautomator}>
+        WhatsApp <br /> Clone
+      </div>
+      <div className={style.anarduinoproject}>
+        The platform enables seamless, secure messaging between users.
+      </div>
+    </div>
+  );
 };
 
 export default Project6;
--- Portfolio/src/Project_7.jsx
+++ Portfolio/src/Project_7.jsx
@@ -1,27 +1,53 @@
 import style from "./Project.module.css";
 
 const Project7 = () => {
-    return (
-        <div className={style.project1}>
-            <div className={style.project1child} />
-            <div className={style.project1item} ><p>Software</p></div>
-            <div className={style.project1inner} />
-            <div className={style.project1child1} > <img src="https://res.cloudinary.com/dkotituzn/image/upload/v1713624654/shahabuddin/tiz1myvnpldzlq9uhx5b.png" alt="" /></div>
-            <div className={style.project1child2} > <video src="https://res.cloudinary.com/dkotituzn/video/upload/v1713619589/shahabuddin/rhcke1motjaaapkqaxx.mp4" controls muted autoPlay /></div>
-            <img className={style.subtracticon} alt="" src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596429/shahabuddin/eng49exy2sdyj5nlxjga.svg" />
-            <img className={style.subtracticon1} alt="" src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596427/shahabuddin/mzbr5u53amxf2w1iwe0o.svg" />
-            <b className={style.project}>Project</b>
-            <div className={style.thiscurtainopener}>{`The platform integrates JWT and NextAuth for secure authentication, advanced filtering for precise product selection, and streamlined  `}</div>
-            <div className={style.openandclose}>
-                bidding, enhancing user experience and facilitating secure SSL Commerz payments.
-            </div>
-            <div className={style.div}>05</div>
-            <div className={style.tempcurtainautomator}>{`Bidding Platform`}</div>
-            <div className={style.anarduinoproject}>
-                A bidding platform with Spring Boot and Next.js for post addition, product selection, bidding, and SSL Commerz-enabled payments.
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.project1}>
+      <div className={style.project1child} />
+      <div className={style.project1item}>
+        <p>Software</p>
+      </div>
+      <div className={style.project1inner} />
+      <div className={style.project1child1}>
+        <img
+          src="https://res.cloudinary.com/dkotituzn/image/upload/v1713624654/shahabuddin/tiz1myvnpldzlq9uhx5b.png"
+          alt=""
+        />
+      </div>
+      <div className={style.project1child2}>
+        <video
+          src="https://res.cloudinary.com/dkotituzn/video/upload/v1714119319/shahabuddin/klwtqgve4mflxfigcrgo.mp4"
+          controls
+          muted
+          autoPlay
+        />
+      </div>
+      <img
+        className={style.subtracticon}
+        alt=""
+        src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596429/shahabuddin/eng49exy2sdyj5nlxjga.svg"
+      />
+      <img
+        className={style.subtracticon1}
+        alt=""
+        src="https://res.cloudinary.com/dkotituzn/image/upload/v1702596427/shahabuddin/mzbr5u53amxf2w1iwe0o.svg"
+      />
+      <b className={style.project}>Project</b>
+      <div
+        className={style.thiscurtainopener}
+      >{`The platform integrates JWT and NextAuth for secure authentication, advanced filtering for precise product selection, and streamlined  `}</div>
+      <div className={style.openandclose}>
+        bidding, enhancing user experience and facilitating secure SSL Commerz
+        payments.
+      </div>
+      <div className={style.div}>05</div>
+      <div className={style.tempcurtainautomator}>{`Bidding Platform`}</div>
+      <div className={style.anarduinoproject}>
+        A bidding platform with Spring Boot and Next.js for post addition,
+        product selection, bidding, and SSL Commerz-enabled payments.
+      </div>
+    </div>
+  );
 };
 
 export default Project7;
